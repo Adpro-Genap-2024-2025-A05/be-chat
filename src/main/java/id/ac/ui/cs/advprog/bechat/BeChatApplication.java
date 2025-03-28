504692291652f613a8decb352a3ed360f92d559a
cleoexcel <cleoxclln@gmail.com>
2025-03-28 17:51:28 +0700

    Initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bechat/BeChatApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bechat/BeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
