d933826c8edfae3de231fbb6688b5bacca323262
cleoexcel <cleoxclln@gmail.com>
2025-04-11 14:46:16 +0700

    [GREEN] enable test BeChatApplicationTests

--- src/test/java/id/ac/ui/cs/advprog/bechat/BeChatApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/bechat/BeChatApplicationTests.java
@@ -1,9 +1,8 @@
 package id.ac.ui.cs.advprog.bechat;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+
 @SpringBootTest
 class BeChatApplicationTests {
 
